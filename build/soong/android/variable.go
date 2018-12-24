AOSiP/platform_vendor_aosip

soong: Rename our android package to android_aosip
Including both android/soong/android and aosp/soong/android
causes one to override the other. This isn't ideal behaviour
so rename to avoid confliction.

Change-Id: I93ab13e9bf4aa83ed79dee36db8dcb56ad215fad

build/soong/android/variable.go
@@ -1,4 +1,4 @@
-package android
+package android_aosip
 type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string

v01dz777/android_vendor_du

build/soong/android/variable.go
@@ -1,4 +1,4 @@
-package android
+package android_du
 type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string